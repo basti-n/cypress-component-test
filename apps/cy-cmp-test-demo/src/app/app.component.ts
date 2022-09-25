9e3a9f3671378c3352103dbf276f30a6ffc63995
Sebastian <sebastian.neumair@gmail.com>
2022-09-25 13:27:26 +0200

    Initial commit

---
+++ apps/cy-cmp-test-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cypress-component-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cy-cmp-test-demo';
+}
